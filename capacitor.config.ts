86d949ff8b1375b73bdd6d56f99493107c3401ea
Jacky Tea <teajacks@outlook.com>
2021-11-08 16:37:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JTea_DPS926_Assignment3',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
